105b9342ab57baeb8bc63ed1d101a9d9687fc5bc
masaaki stephane <62558026+Stephane226@users.noreply.github.com>
2020-07-22 17:11:10 +0300

    1ERE FUNCTION ACCOMPLIE

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-//Burkina faso
-document.getElementById("suu").addEventListener("mouseover", function(){
-    document.getElementById("suu").style.fill = "white";
+//Function on mouse oversss & mouseout for BG CLRS
 
-
+var initialColor0 = document.getElementById("lovebf").style.fill;
+document.getElementById("lovebf").addEventListener("mouseover", function(){
+    var initialColor = document.getElementById("lovebf").style.fill;
+    document.getElementById("lovebf").style.fill = "white";
+    document.getElementById("burkina").style.backgroundColor = initialColor;
 })
+document.getElementById("lovebf").addEventListener("mouseout", function(){
+
+    var initialColor = document.getElementById("lovebf").style.fill;
+    document.getElementById("lovebf").style.fill = initialColor0;
+    document.getElementById("burkina").style.backgroundColor = "#444";
+})
+//Fin De La Founction
