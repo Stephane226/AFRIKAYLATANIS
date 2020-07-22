74db7eb5c20ee611001d56d7efd7d0484eaa2cf7
masaaki stephane <62558026+Stephane226@users.noreply.github.com>
2020-07-22 18:19:54 +0300

    stilll

--- script.js
+++ script.js
@@ -7,9 +7,44 @@ document.getElementById("lovebf").addEventListener("mouseover", function(){
     document.getElementById("burkina").style.backgroundColor = initialColor;
 })
 document.getElementById("lovebf").addEventListener("mouseout", function(){
-
-    var initialColor = document.getElementById("lovebf").style.fill;
     document.getElementById("lovebf").style.fill = initialColor0;
     document.getElementById("burkina").style.backgroundColor = "#444";
 })
+                       //Function de la Liste Mouse over & Out Related to maps
+document.getElementById("burkina").addEventListener("mouseover", function(){
+    
+    document.getElementById("lovebf").style.fill = "white";
+})
+document.getElementById("burkina").addEventListener("mouseout", function(){
+ document.getElementById("lovebf").style.fill = initialColor0;
+
+})
+
+
+//Fin De La Founction
+
+//Function on mouse oversss & mouseout for BG CLRS
+
+var initialColor01 = document.getElementById("algerie").style.fill;
+document.getElementById("algerie").addEventListener("mouseover", function(){
+    var initialColor1 = document.getElementById("algerie").style.fill;
+    document.getElementById("algerie").style.fill = "white";
+    document.getElementById("cez").style.backgroundColor = initialColor1;
+    
+})
+document.getElementById("algerie").addEventListener("mouseout", function(){
+    document.getElementById("algerie").style.fill = initialColor01;
+    document.getElementById("cez").style.backgroundColor = "#444";
+})
+                       //Function de la Liste Mouse over & Out Related to maps
+document.getElementById("cez").addEventListener("mouseover", function(){
+    
+    document.getElementById("algerie").style.fill = "white";
+})
+document.getElementById("cez").addEventListener("mouseout", function(){
+ document.getElementById("algerie").style.fill = initialColor0;
+
+})
+
+
 //Fin De La Founction
