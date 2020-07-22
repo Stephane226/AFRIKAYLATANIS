598766c1ef312dc17695e2c5ca2efdc264188b9a
masaaki stephane <62558026+Stephane226@users.noreply.github.com>
2020-07-22 19:14:46 +0300

    mise en place de 4 5 elements

--- script.js
+++ script.js
@@ -48,3 +48,84 @@ document.getElementById("cez").addEventListener("mouseout", function(){
 
 
 //Fin De La Founction
+
+
+//Function on mouse oversss & mouseout for BG CLRS
+
+var initialColor02 = document.getElementById("angola").style.fill;
+document.getElementById("angola").addEventListener("mouseover", function(){
+    var initialColor2 = document.getElementById("angola").style.fill;
+    document.getElementById("angola").style.fill = "white";
+    document.getElementById("ang").style.backgroundColor = initialColor2;
+})
+document.getElementById("angola").addEventListener("mouseout", function(){
+    document.getElementById("angola").style.fill = initialColor02;
+    document.getElementById("ang").style.backgroundColor = "#444";
+})
+                       //Function de la Liste Mouse over & Out Related to maps
+document.getElementById("ang").addEventListener("mouseover", function(){
+    
+    document.getElementById("angola").style.fill = "white";
+})
+document.getElementById("ang").addEventListener("mouseout", function(){
+ document.getElementById("angola").style.fill = initialColor02;
+
+})
+
+//Function on mouse oversss & mouseout for BG CLRS
+
+var initialColor03 = document.getElementById("benin").style.fill;
+document.getElementById("benin").addEventListener("mouseover", function(){
+    var initialColor3 = document.getElementById("benin").style.fill;
+    document.getElementById("benin").style.fill = "white";
+    document.getElementById("ben").style.backgroundColor = initialColor3;
+})
+document.getElementById("benin").addEventListener("mouseout", function(){
+    document.getElementById("benin").style.fill = initialColor03;
+    document.getElementById("ben").style.backgroundColor = "#444";
+})
+                       //Function de la Liste Mouse over & Out Related to maps
+document.getElementById("ben").addEventListener("mouseover", function(){
+    
+    document.getElementById("benin").style.fill = "white";
+})
+document.getElementById("ben").addEventListener("mouseout", function(){
+ document.getElementById("benin").style.fill = initialColor03;
+
+})
+
+//Function on mouse oversss & mouseout for BG CLRS
+
+var initialColor04 = document.getElementById("botswana").style.fill;
+document.getElementById("botswana").addEventListener("mouseover", function(){
+    var initialColor4 = document.getElementById("botswana").style.fill;
+    document.getElementById("botswana").style.fill = "white";
+    document.getElementById("bot").style.backgroundColor = initialColor4;
+})
+document.getElementById("botswana").addEventListener("mouseout", function(){
+    document.getElementById("botswana").style.fill = initialColor04;
+    document.getElementById("bot").style.backgroundColor = "#444";
+})
+                       //Function de la Liste Mouse over & Out Related to maps
+document.getElementById("bot").addEventListener("mouseover", function(){
+    
+    document.getElementById("botswana").style.fill = "white";
+})
+document.getElementById("bot").addEventListener("mouseout", function(){
+ document.getElementById("botswana").style.fill = initialColor04;
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
